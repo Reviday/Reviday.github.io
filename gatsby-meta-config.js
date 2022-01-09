667e72893b5d1452e60355ec6d7c846f1dc20052
reviday <reviday@naver.com>
2022-01-09 18:54:47 +0900

    fixed untracked files

---
+++ gatsby-meta-config.js
@@ -0,0 +1,88 @@
+module.exports = {
+  title: `reviday`,
+  description: `test`,
+  language: `ko`, // `ko`, `en` => currently support versions for Korean and English
+  siteUrl: `https://reviday.github.io/`,
+  ogImage: `/og-image.png`, // Path to your in the 'static' folder
+  comments: {
+    utterances: {
+      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+    },
+  },
+  ga: '0', // Google Analytics Tracking ID
+  author: {
+    name: `윤성규`,
+    bio: {
+      role: `개발자`,
+      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      thumbnail: 'sample.png', // Path to the image in the 'asset' folder
+    },
+    social: {
+      github: ``, // `https://github.com/zoomKoding`,
+      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
+      email: ``, // `zoomkoding@gmail.com`,
+    },
+  },
+
+  // metadata for About Page
+  about: {
+    timestamps: [
+      // =====       [Timestamp Sample and Structure]      =====
+      // ===== 🚫 Don't erase this sample (여기 지우지 마세요!) =====
+      {
+        date: '',
+        activity: '',
+        links: {
+          github: '',
+          post: '',
+          googlePlay: '',
+          appStore: '',
+          demo: '',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      // {
+      //   date: '2021.02 ~',
+      //   activity: '개인 블로그 개발 및 운영',
+      //   links: {
+      //     post: '/gatsby-starter-zoomkoding-introduction',
+      //     github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+      //     demo: 'https://www.zoomkoding.com',
+      //   },
+      // },
+    ],
+
+    projects: [
+      // =====        [Project Sample and Structure]        =====
+      // ===== 🚫 Don't erase this sample (여기 지우지 마세요!)  =====
+      {
+        title: '',
+        description: '',
+        techStack: ['', ''],
+        thumbnailUrl: '',
+        links: {
+          post: '',
+          github: '',
+          googlePlay: '',
+          appStore: '',
+          demo: '',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        title: '테스트 게시글 입니다.',
+        description:
+          '테스트 게시글 입니다.',
+        techStack: ['gatsby', 'react'],
+        thumbnailUrl: 'blog.png',
+        links: {
+          post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          demo: 'https://www.zoomkoding.com',
+        },
+      },
+    ],
+  },
+};
